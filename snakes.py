b51336e4930c68705f16b0bcb1225ddfdec9fc79
flisnes <haakonflisnes@gmail.com>
2023-06-08 14:19:25 +0200

    Game over screen added, but not showing

--- snakes.py
+++ snakes.py
@@ -14,6 +14,7 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 # Define the size of each grid cell
 cell_size = 20
@@ -24,7 +25,6 @@ grid_height = height // cell_size
 
 # Set up the clock to control the frame rate
 clock = pygame.time.Clock()
-fps = 5  # Lower value for slower game
 
 # Define the initial state of the game
 snake1 = [(grid_width // 4, grid_height // 2)]
@@ -33,13 +33,23 @@ food = (random.randint(0, grid_width - 1), random.randint(0, grid_height - 1))
 direction1 = "RIGHT"
 direction2 = "LEFT"
 
+# Define the game state
+game_over = False
+winner = None
+
+# Function to display game over message
+def display_game_over():
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Player {winner} wins!", True, WHITE)
+    text_rect = text.get_rect(center=(width // 2, height // 2))
+    window.blit(text, text_rect)
+
 # Game loop
-running = True
-while running:
+while not game_over:
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            game_over = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and direction1 != "DOWN":
                 direction1 = "UP"
@@ -59,57 +69,62 @@ while running:
                 direction2 = "RIGHT"
 
     # Update snake positions
-    head1 = snake1[0]
-    if direction1 == "UP":
-        new_head1 = (head1[0], head1[1] - 1)
-    elif direction1 == "DOWN":
-        new_head1 = (head1[0], head1[1] + 1)
-    elif direction1 == "LEFT":
-        new_head1 = (head1[0] - 1, head1[1])
-    elif direction1 == "RIGHT":
-        new_head1 = (head1[0] + 1, head1[1])
+    if not game_over:
+        head1 = snake1[0]
+        if direction1 == "UP":
+            new_head1 = (head1[0], head1[1] - 1)
+        elif direction1 == "DOWN":
+            new_head1 = (head1[0], head1[1] + 1)
+        elif direction1 == "LEFT":
+            new_head1 = (head1[0] - 1, head1[1])
+        elif direction1 == "RIGHT":
+            new_head1 = (head1[0] + 1, head1[1])
 
-    head2 = snake2[0]
-    if direction2 == "UP":
-        new_head2 = (head2[0], head2[1] - 1)
-    elif direction2 == "DOWN":
-        new_head2 = (head2[0], head2[1] + 1)
-    elif direction2 == "LEFT":
-        new_head2 = (head2[0] - 1, head2[1])
-    elif direction2 == "RIGHT":
-        new_head2 = (head2[0] + 1, head2[1])
+        head2 = snake2[0]
+        if direction2 == "UP":
+            new_head2 = (head2[0], head2[1] - 1)
+        elif direction2 == "DOWN":
+            new_head2 = (head2[0], head2[1] + 1)
+        elif direction2 == "LEFT":
+            new_head2 = (head2[0] - 1, head2[1])
+        elif direction2 == "RIGHT":
+            new_head2 = (head2[0] + 1, head2[1])
 
-    # Check for collisions
-    if (
-        new_head1[0] < 0
-        or new_head1[0] >= grid_width
-        or new_head1[1] < 0
-        or new_head1[1] >= grid_height
-        or new_head1 in snake1
-        or new_head1 in snake2
-    ) or (
-        new_head2[0] < 0
-        or new_head2[0] >= grid_width
-        or new_head2[1] < 0
-        or new_head2[1] >= grid_height
-        or new_head2 in snake2
-        or new_head2 in snake1
-    ):
-        running = False
+        # Check for collisions with walls and other snakes
+        if (
+            new_head1[0] < 0
+            or new_head1[0] >= grid_width
+            or new_head1[1] < 0
+            or new_head1[1] >= grid_height
+            or new_head1 in snake1
+            or new_head1 in snake2
+        ):
+            game_over = True
+            winner = 2
+        elif (
+            new_head2[0] < 0
+            or new_head2[0] >= grid_width
+            or new_head2[1] < 0
+            or new_head2[1] >= grid_height
+            or new_head2 in snake2
+            or new_head2 in snake1
+        ):
+            game_over = True
+            winner = 1
 
-    # Check if the snakes have eaten the food
-    if new_head1 == food:
-        food = (random.randint(0, grid_width - 1), random.randint(0, grid_height - 1))
-    else:
-        snake1.pop()
+        # Check if the snakes have eaten the food
+        if new_head1 == food:
+            food = (random.randint(0, grid_width - 1), random.randint(0, grid_height - 1))
+        else:
+            snake1.pop()
 
-    if new_head2 == food:
-        food = (random.randint(0, grid_width - 1), random.randint(0, grid_height - 1))
-    else:
-        snake2.pop()
+        if new_head2 == food:
+            food = (random.randint(0, grid_width - 1), random.randint(0, grid_height - 1))
+        else:
+            snake2.pop()
 
-    snake1.insert(0, new_head1)
-    snake2.insert(0, new_head2)
+        snake1.insert(0, new_head1)
+        snake2.insert(0, new_head2)
 
     # Clear the window
     window.fill(BLACK)
@@ -133,7 +148,14 @@ while running:
     pygame.display.flip()
 
     # Control the frame rate
-    clock.tick(fps)
+    clock.tick(10)
 
-# Quit the game
-pygame.quit()
+# Display the winner
+display_game_over()
+
+# Keep the game window open until the user closes it
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
