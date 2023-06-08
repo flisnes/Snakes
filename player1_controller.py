b6ba7f3f7835e70e5bcb1ae3dfe18da02aa75594
flisnes <haakonflisnes@gmail.com>
2023-06-08 15:13:11 +0200

    Changed movement to scripts (not playable)

---
+++ player1_controller.py
@@ -0,0 +1,5 @@
+def move(snake1, snake2, food):
+    # Implement your player 1 controller logic here
+    # You have access to the position of snake1, snake2, and food
+    # Return the desired direction of movement for snake1 as dx, dy
+    return dx, dy
---
+++ player_controller.py
@@ -0,0 +1,5 @@
+def move(snake1, snake2, food):
+    # Implement your player controller logic here
+    # You have access to the position of snake1, snake2, and food
+    # Return the desired direction of movement for snake2 as dx, dy
+    return dx, dy
