d6d3ebaaa01932d5352c055fd7658f80e02992b3
flisnes <haakonflisnes@gmail.com>
2023-06-13 01:30:04 +0200

    Cleanup

--- snake_controller_template.py
+++ snake_controller_template.py
@@ -1,6 +1,6 @@
 def move(your_snake, opponent_snake, food, previous_direction):
     # Implement your player controller logic here
     # You have access to the position of your_snake, opponent_snake, food and your previous_direction.
-    # Return the desired direction of movement for snake1 as dx, dy
+    # Return the desired direction of movement for your snake as "UP", "DOWN", "LEFT" or "RIGHT".
 
     return previous_direction
